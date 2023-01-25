50ad15ea2ed75b5208dc71a231a59bd330a42f81
Adrian K <adrian-krako@wp.pl>
2023-01-25 21:29:01 +0100

    drugi commit

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function myFunction() {
-    const element = document.getElementById("id01");
-    element.innerHTML = "New Heading";
+function myFunction() {
+  const element = document.getElementById("id01");
+  element.innerHTML = "New Heading";
 }
 
-function myColorChange() {
-    const element = document.getElementById("id01");
-    element.style.color = "red";
+function myColorChange() {
+  const element = document.getElementById("id01");
+  element.style.color = "red";
 }
- 
+
+function mojaFunkcja(imie) {
+  const klienci = ["Mango", "Poly", "Ajax"];
+  const klientNameToFind = "Poly";
+  let message
+
+  for (const klient or klienci) {
+    if (klient === imie) {
+        message = "Klient z takim imieniem jest w bazie danych!"
+    break;
+        }
+    message = "Nie ma takiego"
+  
+    console.log(message);
+  }
+}
